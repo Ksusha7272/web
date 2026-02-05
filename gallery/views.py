205d070873d1a83ba87540fbd378b1401335c90d
Ksusha7272 <sherstobitova2020@bk.ru>
2026-02-05 14:18:27 +0700

    Week 1: Added templates and mock data loop

---
+++ gallery/views.py
@@ -0,0 +1,22 @@
+
+from django.http import HttpResponse
+
+from django.shortcuts import render
+# HttpResponse нам больше не нужен, render делает это за нас
+def home(request):
+    # Имитация данных из базы (список словарей)
+    fake_database = [
+        {'id': 1, 'name': 'Sci-Fi Helmet', 'file_size': '15 MB'},
+        {'id': 2, 'name': 'Old Chair', 'file_size': '2 MB'},
+        {'id': 3, 'name': 'Cyber Truck', 'file_size': '10 MB'},
+        {'id': 4, 'name': 'Table', 'file_size': '7 MB'},
+    ]
+    context_data = {
+        'page_title': 'Главная Галерея',
+        'assets': fake_database, # Передаем весь список
+    }
+    return render(request, 'gallery/index.html', context_data)
+
+def about(request):
+# Мы пока не используем HTML-шаблоны, просто вернем строку.
+    return HttpResponse("<h1>Курс Web Структуры.</p>")
